b32993c33335a62d8a5e36c3df53592dc660f059
Jasonlin1198 <jasonlin1198@gmail.com>
2020-03-20 15:49:11 -0700

    map

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import React from 'react';
-
+import React, { Component } from 'react';
+import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
        <h1>Welcome to our web app</h1>
+
+
     </div>
   );
 }
 
-export default App;
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
+export class MapContainer extends Component {
+  render() {
+    return (
+      <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyCVCnlvTmnjOYLcbHE6jKnPLq9hb1UtThY'
+})(MapContainer);
+
