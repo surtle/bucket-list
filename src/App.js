872b444a73bfc1816a8bf03d4edd6ecee38a9420
silvia <surtle@me.com>
2020-03-21 12:30:33 -0700

    added firebase setup

--- src/App.js
+++ src/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import { Map, GoogleApiWrapper } from 'google-maps-react';
 import './App.css';
 
+
+// ===================== FIREBASE SETUP ============================
+
+var firebase = require("firebase/app");
+
+require("firebase/auth");
+require("firebase/database");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAj52JUY4jm-SQU5td6zPyiBNWyX0x-5l4",
+  authDomain: "bucketlist-c95a0.firebaseapp.com",
+  databaseURL: "https://bucketlist-c95a0.firebaseio.com",
+  projectId: "bucketlist-c95a0",
+  storageBucket: "bucketlist-c95a0.appspot.com",
+  messagingSenderId: "300377193585",
+  appId: "1:300377193585:web:b2d856768f2dc4f6d36fd0",
+  measurementId: "G-GX1SX7KKYV"
+};
+
+// =================================================================
+
+const mapStyles = {
+  width: '100%',
+  height: '100%'
+};
+
 function App() {
   return (
     <div className="App">
@@ -10,17 +36,20 @@ function App() {
       </div>
 
       <div class="map">
-        <p> map placeholder </p>
+        <Map
+        google={this.props.google}
+        zoom={14}
+        style={mapStyles}
+        initialCenter={{
+         lat: -1.2884,
+         lng: 36.8233
+        }}
+      />
       </div>
     </div>
   );
 }
 
-
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
 
 export class MapContainer extends Component {
   render() {
