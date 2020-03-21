a9725cb154a977d9b41ea835f2a874b06847b7b6
Jasonlin1198 <jasonlin1198@gmail.com>
2020-03-21 13:08:52 -0700

    geolocation

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
 import React, { Component } from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import ReactDOM from 'react-dom';
 import './App.css';
+import MapContainer from './MapContainer';
+import FlexView from 'react-flexview';
 
 function App() {
+  
   return (
     <div className="App">
       <div class="header">
         <a href = "/" class="title">bucketlist</a>
       </div>
 
-      <div class="map">
-        <p> map placeholder </p>
+
+      <div class = "list" >
+        <ul class = "menu">
+            <li>Sign in</li>
+            <li>About</li>
+            <li>My Itinerary</li> 
+        </ul>
       </div>
+      
+      <div>
+        <MapContainer/>
+      </div>
+      
+      
+
     </div>
   );
+ 
 }
 
 
-const mapStyles = {
-  width: '100%',
-  height: '100%'
-};
 
-export class MapContainer extends Component {
-  render() {
-    return (
-      <Map
-        google={this.props.google}
-        zoom={14}
-        style={mapStyles}
-        initialCenter={{
-         lat: -1.2884,
-         lng: 36.8233
-        }}
-      />
-    );
-  }
-}
-
-export default GoogleApiWrapper({
-  apiKey: 'AIzaSyCVCnlvTmnjOYLcbHE6jKnPLq9hb1UtThY'
-})(MapContainer);
-
+export default App
---
+++ src/MapContainer.js
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import { GoogleApiWrapper, InfoWindow, Marker } from "google-maps-react";
+import "./App.css";
+import CurrentLocation from "./Map";
+
+const mapStyles = {
+  width: "100%",
+  height: "100%"
+};
+
+export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+  };
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+  render() {
+    return (
+      <div>
+        <CurrentLocation
+          centerAroundCurrentLocation
+          google={this.props.google}
+          zoom={14}
+          style={mapStyles}
+        >
+          <Marker onClick={this.onMarkerClick} name={"current location"} />
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
+          >
+            <div>
+              <h4>{this.state.selectedPlace.name}</h4>
+            </div>
+          </InfoWindow>
+          
+        </CurrentLocation>
+
+      </div>
+    );
+  }
+}
+
+export default GoogleApiWrapper({
+  apiKey: "AIzaSyCVCnlvTmnjOYLcbHE6jKnPLq9hb1UtThY"
+})(MapContainer);
